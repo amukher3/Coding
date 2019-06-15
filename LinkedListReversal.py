1be4dce26dbd40c56ad1100fcd2d53bec0d43533
Abhishek Mukherjee <51139088+amukher3@users.noreply.github.com>
2019-06-15 19:38:02 -0400

    Implementation of Linked List Reversal

---
+++ LinkedListReversal.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 15 19:32:25 2019
+
+@author: Abhishek Mukherjee
+"""
+
+def reverse(head):
+    
+    # Set up current,previous, and next nodes
+    current = head
+    previous = None
+    nextnode = None
+
+    # until we have gone through to the end of the list
+    while current:
+        
+        
+        # Before overwriting as the previous node for reversal
+        nextnode = current.nextnode
+
+        # Reverse the pointer ot the next_node
+        current.nextnode = previous
+
+        # Go one forward in the list
+        previous = current
+        current = nextnode
+
+    return previous
