84b20acd1c4e9c0cffb9af031201977dbfd09d77
Abhishek Mukherjee <51139088+amukher3@users.noreply.github.com>
2019-08-13 18:42:10 -0400

    Add files via upload

---
+++ HackerRank_PowerMod.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  5 18:31:31 2019
+
+@author: amukher3
+"""
+
+#input a
+a=int(input())
+
+#input b
+b=int(input())
+
+#input m
+m=int(input())
+
+power=pow(a,b)
+
+power_mod=pow(a,b,m)
+
+print(power)
+
+print(power_mod)
