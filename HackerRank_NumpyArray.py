7354b96eb2874a54de8ebb170338d8ffc219256c
Abhishek Mukherjee <51139088+amukher3@users.noreply.github.com>
2019-08-13 20:18:09 -0400

    Add files via upload

---
+++ HackerRank_NumpyArray.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 13 19:48:53 2019
+
+@author: amukher3
+"""
+
+import numpy as np
+
+lst=list(map(int,input().split()))
+
+a=np.array(arr,float)
+
+a=np.flip(a,0)
