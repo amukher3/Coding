f5af2fac5df9606d65f0a81825af2635cd865920
Abhishek Mukherjee <51139088+amukher3@users.noreply.github.com>
2020-01-02 23:22:12 -0800

    IK practice set --TopK problem

--- IK_TopK.py
+++ IK_TopK.py
@@ -7,13 +7,21 @@ Created on Fri Dec 27 18:44:07 2019
 
 ## Top K 
 ##IK practice set...
+
+arr=[16,5,7,10,2,2,4,4,9,9,4,10,2,6,7,3,8,7]
+
 def topk(arr,k):
     
     #size of array is the first element
-    #Removing the length entry
+    #Removing the length entry.
+    
+    #N.B: If you are usinig the IK web
+    #Interface you can comment the next line
+    # otherwise remove the first element which is
+    #the length of the array.
     arr=arr[1:]
     
-    #Removing duplicates, if any
+    #Removing duplicates,if any
     arr=list(set(arr))
     
     #let's do a sort now
