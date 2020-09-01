9a756ca942f31002ce7145320aa5af4a05e9cf4b
Abhishek Mukherjee <abhi0787@gmail.com>
2020-09-01 13:10:31 -0700

    LeetCode_SearchInaPivotedArray

---
+++ LeetCode_SearchInaPivotedArray.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 31 16:51:20 2020
+
+@author: Abhishek Mukherjee
+"""
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        low=0
+        high=len(nums)-1
+        
+        while low<=high:
+            mid=(low+high)//2
+            
+            if nums[mid]==target:
+                return mid
+            
+            if nums[low]<=nums[mid]:
+                if nums[low]<=target and nums[mid]>=target:
+                    high=mid-1
+                else:
+                    low=mid+1
+            else:
+                if nums[mid]<=target and nums[high]>=target:
+                    low=mid+1
+                else:
+                    high=mid-1
+                           
+        return -1
+        
